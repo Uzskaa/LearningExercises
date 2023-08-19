a0eeaf66254f23f234cc71c30b9e6a75c8a7e03f
Uzskaa <142003814+Uzskaa@users.noreply.github.com>
2023-08-19 13:52:34 +0800

    Date & Time

---
+++ ex08.py
@@ -0,0 +1,9 @@
+# Title: Date & Time
+# This program prints the current date and time!
+
+import datetime
+
+now = datetime.datetime.now()
+
+print('Current date and time: ')
+print(now.strftime('%Y-%m-%d %H:%M:%S'))
