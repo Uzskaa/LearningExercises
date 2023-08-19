7950c48b585fe6a6009dba2386333345ebd6a8f4
Uzskaa <142003814+Uzskaa@users.noreply.github.com>
2023-08-19 13:54:24 +0800

    Reverse name with a space

---
+++ ex10.py
@@ -0,0 +1,7 @@
+# Title: Reverse the name with a space
+# This program  accepts the user's first and last name and prints them in reverse order with a space between them.
+
+fname = input('Enter First Name: ')
+lname = input('Enter Last Name: ')
+
+print('Hello '+ lname, fname)
