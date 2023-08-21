c81d4525a82a7af6459f90607253e294108cf1db
Uzskaa <142003814+Uzskaa@users.noreply.github.com>
2023-08-21 15:12:49 +0800

    Exam Date

---
+++ ex14.py
@@ -0,0 +1,11 @@
+# Title: Exam Date
+# This program displays the examination schedule. (extract the date from exam_st_date)
+
+
+exam_st_date = (11, 12, 2014)
+month = exam_st_date[0]
+day = exam_st_date[1]
+year = exam_st_date[-1]
+
+print('The examination will be on '+str(month)+'/'+str(day)+'/'+str(year))
+
