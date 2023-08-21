69eb059f660537a676cfaedd299b1704270db342
Uzskaa <142003814+Uzskaa@users.noreply.github.com>
2023-08-21 15:12:05 +0800

    First and Last

---
+++ ex13.py
@@ -0,0 +1,5 @@
+# Title: First and Last
+# This program displays the first and last colors from the following list.
+
+color_list = ["Red","Green","White" ,"Black"]
+print( "%s %s"%(color_list[0],color_list[-1]))
