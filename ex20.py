10fb3e2d0e6e9510159958d4d8c7f800ba584979
Uzskaa <142003814+Uzskaa@users.noreply.github.com>
2023-08-22 15:33:59 +0800

    Volume of a sphere

---
+++ ex20.py
@@ -0,0 +1,9 @@
+# Title: Volume of a sphere
+# This program gets the volume of a sphere with radius six.
+
+import math
+
+r= 6.0
+V= 4.0/3.0* math.pi* r**3
+
+print('The volume of the sphere is: ',V)
