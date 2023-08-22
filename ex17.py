c6d21f92646b9e69c2a5fd6ffd98bf4ae2a89e81
Uzskaa <142003814+Uzskaa@users.noreply.github.com>
2023-08-22 15:32:58 +0800

    Calendar

---
+++ ex17.py
@@ -0,0 +1,7 @@
+# Title: Calendar
+# This program prints the calendar for a given month and year.
+
+import calendar
+y = int(input("Input the year : "))
+m = int(input("Input the month : "))
+print(calendar.month(y, m))
