c3041ae82bc8f36906a79ea592592ca485dcd23e
Uzskaa <142003814+Uzskaa@users.noreply.github.com>
2023-08-22 15:33:19 +0800

    multiline strings

---
+++ ex18.py
@@ -0,0 +1,9 @@
+# Title: multiline strings
+# This program prints using the multiline string
+
+print("""
+a string that you "don't" have to escape
+This
+is a  ....... multi-line
+heredoc string --------> example
+""")
