6d089d77c561a3d98be0fe7b15aa21cb1b849f3e
Uzskaa <142003814+Uzskaa@users.noreply.github.com>
2023-08-22 15:32:36 +0800

    Print the documents of Python built-in function(s)

---
+++ ex16.py
@@ -0,0 +1,6 @@
+# Title: Print the documents of Python built-in function(s)
+# This program prints the documents (syntax, description etc.) of Python built-in function(s).
+# Sample function: abs()
+
+print(abs.__doc__)
+
