6686cc6519b42f80ca24e0606319df0db95acdf7
Uzskaa <142003814+Uzskaa@users.noreply.github.com>
2023-08-26 16:20:52 +0800

    Check whether a specified value is contained in a group of values

---
+++ ex29.py
@@ -0,0 +1,15 @@
+# Title: Count vowels in python using function
+# This program tests whether a passed letter is a vowel or not.
+
+def isVowel(char):
+    vowels = "aeiou"
+    for n in vowels:
+        if n == char:
+            isit = "VOWEL"
+            break
+        else:
+            isit = "CONSONANT"
+    print("the character is a", isit)
+
+userInput = input("input a character: ")  
+isVowel(userInput)
