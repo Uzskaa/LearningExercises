ccc786b5d37325938693efe300b766acef715555
Uzskaa <142003814+Uzskaa@users.noreply.github.com>
2023-08-26 16:22:21 +0800

    Count vowels in python using function

--- ex29.py
+++ ex29.py
@@ -1,6 +1,7 @@
 # Title: Count vowels in python using function
 # This program tests whether a passed letter is a vowel or not.
 
+
 def isVowel(char):
     vowels = "aeiou"
     for n in vowels:
@@ -10,6 +11,7 @@ def isVowel(char):
         else:
             isit = "CONSONANT"
     print("the character is a", isit)
+
 
 userInput = input("input a character: ")  
 isVowel(userInput)
